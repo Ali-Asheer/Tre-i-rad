0dc5630422186d2197a1f68d0d64a41094f51b43
Ali-Asheer <Ali_Asheer@hotmail.com>
2024-11-21 00:03:30 +0100

    update a main game

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-
 public class Game {
 
     int[] partSelect = new int[9];
@@ -12,16 +11,14 @@ public class Game {
     private final int USER1 = 1;
     private final int USER2 = 2;
     private final int COMPUTER = 2;
-    private int user1Score = 0;
-    private int user2Score = 0;
-
-
+    private int p1Score = 0;
+    private int p2Score = 0;
 
 
     //  Start of game
 
     public Game() {
-    
+
         PlayersMenuText();                                                            // To first page menu
         int gameSelect = askNumber1("", 2);                            // To select between player against player or player against computer
         TwoPlayersMenu(gameSelect);                                                   // To input menu to input players names
@@ -29,54 +26,55 @@ public class Game {
     }
 
 
-
     // Main game start
 
-    public void gameStart(String p1, String p2) {
-        System.out.println("( " + p1 + "'s Score : " + user1Score + "  )  VS  ( " + p2 + "'s Score : " + user2Score + " )");
+    public void gameStart(String p1Name, String p2Name) {
+        System.out.println("( " + p1Name + "'s Score : " + p1Score + "  )  VS  ( " + p2Name + "'s Score : " + p2Score + " )");
         System.out.println("--------------------------------------------------------------------------------------------------------");
         makeEmptyArray();               // To make array with 9 cells to put 1 for player 1 and 2 for player 2 and 0 if it empties.
         DrawTable mt = new DrawTable();
         char[][] mainArr = mt.mainArray();
-        printArray(mainArr);
+        char[][] xArr = mt.xArray();
+        char[][] oArr = mt.oArray();
+        printArray(mainArr);            // Draw a main board
         boolean running = false;
 
         while (!running) {
-            while (!isArrayFull()) {
-                twoPlayersGame(askNumber2(p1, USER1), mainArr, mt.xArray());
-                if (isWon(USER1, p1, p1, p2)) {
-                    getYesNo(p1, p2);
-                    running = true; }
+            while (!isArrayFull()) {          // running the game until the board become full
 
-                if (isArrayFull()) {
-                    System.out.println("********************************");
-                    System.out.println("**( The game ended in a draw )**");
-                    System.out.println("********************************");
-                    System.out.println();
-                    getYesNo(p1, p2); }
-
-                if (Objects.equals(p2, "Computer")) {
-                    twoPlayersGame(computerNum(), mainArr, mt.oArray());
-                } else {
-                    twoPlayersGame(askNumber2(p2, USER2), mainArr, mt.oArray()); }
-
-                if (isWon(USER2, p2, p1, p2)) {
-                    getYesNo(p1, p2);
+                twoPlayersGame(askNumber2(p1Name, USER1), mainArr, xArr);
+                if (isWon(USER1, p1Name, p1Name, p2Name)) {
+                    getYesNo(p1Name, p2Name);
                     running = true;
-                    break;}
+                    break;
+                }
 
                 if (isArrayFull()) {
-                    System.out.println("********************************");
-                    System.out.println("**( The game ended in a draw )**");
-                    System.out.println("********************************");
-                    System.out.println();
-                    getYesNo(p1, p2);
+                    getYesNo(p1Name, p2Name);
+                }
+
+                if (Objects.equals(p2Name, "Computer")) {
+                    twoPlayersGame(computerNum(), mainArr, oArr);
+                } else {
+                    twoPlayersGame(askNumber2(p2Name, USER2), mainArr, oArr);
+                }
+
+                if (isWon(USER2, p2Name, p1Name, p2Name)) {
+                    getYesNo(p1Name, p2Name);
+                    running = true;
+                    break;
+                }
+
+                if (isArrayFull()) {
+                    getYesNo(p1Name, p2Name);
                 }
 
             }
-            System.out.println("*********** GAME OVER **********");
+
             break;
         }
+
+        System.out.println("*********** GAME OVER **********");
     }
 
     // Location of symbol array on a main array
@@ -159,59 +157,58 @@ public class Game {
 
 // menu to input players names
 
-public void TwoPlayersMenu(int gameSelect) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println();
-    System.out.print("Enter Player 1 Name : ");
-    String p1 = sc.next();
-
-    String p2;
-    if (gameSelect == 1) {
-        System.out.print("Enter Player 2 Name : ");
-        p2 = sc.next();
-    } else p2 = "Computer";
-    System.out.println();
-    System.out.println("( This game is between " + p1 + " VS " + p2 + " )");
-    System.out.println("( " + p1 + " will Start the game with (X) symbol and " + p2 + " with symbol (O) )");
-    System.out.println();
-    getYesNo(p1, p2);           // to select to continue or exit
-}
+    public void TwoPlayersMenu(int gameSelect) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Enter Player 1 Name : ");
+        String p1Name = sc.next();
+        String p2Name;
+        if (gameSelect == 1) {
+            System.out.print("Enter Player 2 Name : ");
+            p2Name = sc.next();
+        } else p2Name = "Computer";
+        System.out.println();
+        System.out.println("( This game is between " + p1Name + " VS " + p2Name + " )");
+        System.out.println("( " + p1Name + " will Start the game with (X) symbol and " + p2Name + " with symbol (O) )");
+        System.out.println();
+        getYesNo(p1Name, p2Name);           // to select to continue or exit
+    }
 
 // First page menu
 
-public void PlayersMenuText() {
-    System.out.println("#####################################");
-    System.out.println("############ TRE I RAD  #############");
-    System.out.println("#####################################");
-    System.out.println();
-    System.out.println("(1) - Two Players");
-    System.out.println("(2) - You VS Computer");
-    System.out.println("----------------------");
-}
+    public void PlayersMenuText() {
+        System.out.println("#####################################");
+        System.out.println("############ TRE I RAD  #############");
+        System.out.println("#####################################");
+        System.out.println();
+        System.out.println("(1) - Two Players");
+        System.out.println("(2) - You VS Computer");
+        System.out.println("----------------------");
+    }
 
 // To select a number and check it if it`s not in the range and not a string.
 
-public int askNumber1(String playerName, int ask) {
-    boolean error = false;
-    int x = 0;
-    if (playerName != "") playerName = playerName + ". ";
-    do {
-        try {
-            do {
-                Scanner sc = new Scanner(System.in);
-                System.out.print(playerName + "Please enter a number between (1-" + ask + ") : ");
-                x = sc.nextInt();
-                error = false;
+    public int askNumber1(String playerName, int ask) {
+        boolean error = false;
+        int x = 0;
+        if (playerName != "") playerName = playerName + ". ";
+        do {
+            try {
+                do {
+                    Scanner sc = new Scanner(System.in);
+                    System.out.print(playerName + "Please enter a number between (1-" + ask + ") : ");
+                    x = sc.nextInt();
+                    error = false;
+                }
+                while (x < 1 || x > ask);
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input..Please input integer only..");
+                error = true;
             }
-            while (x < 1 || x > ask);
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input..Please input integer only..");
-            error = true;
         }
+        while (error);
+        return (x);
     }
-    while (error);
-    return (x);
-}
 
 // select to continue or exit
 
@@ -223,7 +220,8 @@ public int askNumber1(String playerName, int ask) {
             System.out.print("Du you want to continue (Y/N) : ");
             String input = scanner.nextLine();
             if (input.toLowerCase().startsWith("j") || input.toLowerCase().startsWith("y")) {
-               gameStart(p1, p2);yesNo = false;                  // if select Yes it will continue.
+                gameStart(p1, p2);
+                yesNo = false;                  // if select Yes it will continue.
             } else if (input.toLowerCase().startsWith("n")) {
                 System.out.println("------- Thank you and goodbye -------");
                 yesNo = false;
@@ -241,13 +239,17 @@ public int askNumber1(String playerName, int ask) {
         }
     }
 
-//  check if all cells are full
+    //  check if all cells are full
     public boolean isArrayFull() {
         for (int i = 0; i < 9; i++) {
             if (partSelect[i] == EMPTY) {
                 return false;
             }
         }
+        System.out.println("********************************");
+        System.out.println("**( The game ended in a draw )**");
+        System.out.println("********************************");
+        System.out.println();
         return true;
     }
 
@@ -270,6 +272,7 @@ public int askNumber1(String playerName, int ask) {
             return false;
         }
     }
+
     // Generate a random number as a computer choice
     public int computerNum() {
         Random random = new Random();
@@ -294,17 +297,17 @@ public int askNumber1(String playerName, int ask) {
                     || (partSelect[0] == player && partSelect[4] == player && partSelect[8] == player)
                     || (partSelect[6] == player && partSelect[4] == player && partSelect[2] == player)) {
                 if (player == 1) {
-                    user1Score++;
+                    p1Score++;
                 }
                 if (player == 2) {
-                    user2Score++;
+                    p2Score++;
                 }
                 System.out.println("**** " + p + " win this round, congratulations. ****");
                 System.out.println("---------------------------------------------------------");
                 return true;
             }
         }
-        System.out.println("( " + p1 + "ّs Score : " + user1Score + "  )  VS  ( " + p2 + "ّs Score : " + user2Score + " )");
+        System.out.println("( " + p1 + "ّs Score : " + p1Score + "  )  VS  ( " + p2 + "ّs Score : " + p2Score + " )");
         return false;
     }
 
