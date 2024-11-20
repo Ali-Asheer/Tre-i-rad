ebfed7511ff61b1f770c274f0bb9c843816019af
Ali-Asheer <Ali_Asheer@hotmail.com>
2024-11-20 22:32:25 +0100

    select to continue or exit

--- src/Game.java
+++ src/Game.java
@@ -3,18 +3,40 @@ import java.util.Scanner;
 
 
 
-
-//  Start Program
-
 public class Game {
 
-    public void Game(String[] args) {                             m
-        PlayersMenuText();
-        int gameSelect = askNumber1("", 2);
-        if (gameSelect == 1) {TwoPlayers();}
-        else if (gameSelect == 2) {onePlayers();
-        }
+    //  Start of game
+
+    public Game() {
+    
+        PlayersMenuText();                                                            // To first page menu
+        int gameSelect = askNumber1("", 2);                            // To select between player against player or player against computer
+        TwoPlayersMenu(gameSelect);                                                   // To input menu to input players names
+
     }
+
+
+
+
+
+// menu to input players names
+
+public void TwoPlayersMenu(int gameSelect) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println();
+    System.out.print("Enter Player 1 Name : ");
+    String p1 = sc.next();
+
+    String p2;
+    if (gameSelect == 1) {
+        System.out.print("Enter Player 2 Name : ");
+        p2 = sc.next();
+    } else p2 = "Computer";
+    System.out.println();
+    System.out.println("( This game is between " + p1 + " VS " + p2 + " )");
+    System.out.println("( " + p1 + " will Start the game with (X) symbol and " + p2 + " with symbol (O) )");
+    System.out.println();
+    getYesNo(p1, p2);
 }
 
 // First page menu
@@ -69,4 +91,34 @@ public int askNumber1(String playerName, int ask) {
     }
     while (error);
     return (x);
+}
+
+    public void getYesNo(String p1, String p2) {
+        Scanner scanner = new Scanner(System.in);
+        boolean yesNo = true;
+
+        do {
+            System.out.print("Du you want to continue (Y/N) : ");
+            String input = scanner.nextLine();
+            if (input.toLowerCase().startsWith("j") || input.toLowerCase().startsWith("y")) {
+                System.out.println("stttaaaarrrrttttttttttt");yesNo = false;
+               // twoPlayersStart(p1, p2);
+            } else if (input.toLowerCase().startsWith("n")) {
+                System.out.println("------- Thank you and goodbye -------");
+                yesNo = false;
+
+            }
+
+        } while (yesNo);
+
+    }
+
+
+
+
+
+
+
+
+
 }
